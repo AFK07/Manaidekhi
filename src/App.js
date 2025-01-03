b2f173e9046e90f4de847f613d1d4479f8c04534
Gananayak Gurung <gananayak2000@gmail.com>
2025-01-03 17:42:14 +0000

    unnecessary files removed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import './App.css'; // Add styles for the background video
+import './App.css';
 import NavBar from './Navbar/NavBar';
 import Slideshow from './Slideshow/Slideshow';
 import Collection from './Collection/Collection';
 import Instagram from './Instagram/Instagram';
-import backvid from './backvid.mp4'; // Import the background video
 
 function App() {
   return (
     <div className="App">
-      {/* Background Video */}
-      {/* <video autoPlay muted loop className="background-video">
-        <source src={backvid} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video> */}
-
-      {/* Content */}
       <div className="content">
         <NavBar />
         <Slideshow />
@@ -27,3 +19,4 @@ function App() {
 }
 
 export default App;
+
